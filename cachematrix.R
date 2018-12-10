6e798ace7a5e7428c0a3dacc37f95b9d5d58997e
nicoflinta <nicoflinta@hotmail.co.uk>
2018-12-10 08:52:50 +0000

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
-
+## This function creates a special "matrix" object that can cache its inverse.
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setmatrix <- function(solve) m <<- solve
+    getmatrix <- function() m
+    list(set = set, get = get,
+             setmatrix = setmatrix,
+             getmatrix = getmatrix)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+## If the inverse has already been calculated (and the matrix has not changed), then the cachesolve 
+## should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    m <- x$getmatrix()
+    if (!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setmatrix(m)
+    m
 }
+
+
+### Run the functions
+
+## Create Matrix and cache it
+my_matrix <- matrix(c(5, 1, 0,
+                   3, -1, 2,
+                   4, 0, -1), nrow = 3, byrow = TRUE)
+matrix_data <- makeCacheMatrix()
+
+matrix_data$set(my_matrix)
+## Validate inverse matrix result (matrix_inverse <- solve(matrix_data))
+cacheSolve(matrix_data)
+
